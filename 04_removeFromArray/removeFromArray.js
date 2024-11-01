ee039092d2dcd4fdac881f2638b188ba2ca94188
Juan Pineda <juancanales5019@gmail.com>
2024-11-01 11:52:47 -0600

    Created an index page to deploy all my solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
 const removeFromArray = function(array,...args) { //here I had to google up how to declare n parameters: with rest ...
-    let newArray = [];
+    let newArray = [];// here will be store the values that are not part of the given arguments...
     array.forEach(element => {
         if (!args.includes(element)){ //check if  the current element of array is not part of the arguments
             newArray.push(element) // if it is not found in the arguments then add element to the new array
