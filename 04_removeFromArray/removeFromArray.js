f825ec8567bd7ac153c624985f1490e1cd2ee088
Juan Pineda <juancanales5019@gmail.com>
2024-11-01 11:26:06 -0600

    Got to a solution but did not pass the 8 test
    with my solution I was able to only remove elements
    of the same type

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) { //here I had to google up how to declare n parameters: with rest ...
+    let arrayFromArguments = args;
+    for (element in arrayFromArguments) {
+        for (i in array){
+            if (arrayFromArguments[element] == array[i]){
+                let position = array.indexOf(array[i]);
+                array.splice(position,1);
+            }
+        }
+    }
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
