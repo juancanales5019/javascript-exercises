b2850a4c5ae4f6625a76ad4b916aea20671ec50d
Juan Pineda <juancanales5019@gmail.com>
2024-10-31 22:43:56 -0600

    completed exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedStrArray = [];
+    let separatedStrArray = Array.from(str);
+    while (separatedStrArray.length >0) {
+        reversedStrArray.push(separatedStrArray.pop());
+    }
+    return reversedStrArray.join("");
 };
-
+console.log(reverseString("HELLO"))
+console.log(reverseString("WHAT ARE YOU DOING"))
 // Do not edit below this line
 module.exports = reverseString;
