ef7e2f2e3c11a1e292b6c3beccd2a3bfbfbd9d55
Juan Pineda <juancanales5019@gmail.com>
2024-11-03 17:14:07 -0600

    Completes exercise 06_leapYears
    though I got to a working answers I think there are
    some mistakes in the npm test answers

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year % 4 == 0 && (!(year % 100 == 0) || year % 400 == 0)){
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(700));
 // Do not edit below this line
 module.exports = leapYears;
