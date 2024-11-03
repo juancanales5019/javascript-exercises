0b037d0feb586ac947faddc6963527c0efc16516
Juan Pineda <juancanales5019@gmail.com>
2024-11-03 12:20:34 -0600

    completed test 05_sum all
    my solution looks longer that the proposed solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if (isNaN(a) || isNaN(b) || a < 0 || b <0 || typeof(a) == "string" || typeof(b) == "string" || !Number.isInteger(a) || !Number.isInteger(b))  {
+        return "ERROR";
+    }else if (a > b ){
+        for (i = b; i <= a; i++){
+            sum += i;
+        }
+    } else {
+        if (a < b){
+            for (i = a;i <= b; i++){
+                sum += i;
+            } 
+        }
+        
+    }
+    return sum;  
 };
-
+let test = sumAll(4,1);
+console.log(test);
 // Do not edit below this line
 module.exports = sumAll;
