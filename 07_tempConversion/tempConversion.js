0a30de08e19573fc9182dc3eeda20a3d7a7e359a
Juan Pineda <juancanales5019@gmail.com>
2024-11-03 18:03:55 -0600

    Completed exercise 07_tempConversion
    I do not know why some test are not working with
    my solution even though it is the same as the
    proposed solution;

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+//°C = (°F - 32) × 5/9
+//Round a number to 1 decimal place
+//Usando Math.round(): Multiplica el número por 10, redondea, y luego divide por 10.
+//Math.round() does not work
+//use toFixed(1) instead
+    let c = (f -32) * (5/9);
+    let rounded = Math.round((c*10)/10);
+    let fixed = parseFloat(c.toFixed(1));
+    return fixed;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  //°F = °C × (9/5) + 32.
+    let f = c * (9/5) +32;
+    let rounded = Math.round((f*10)/10);
+    let fixed = parseFloat(f.toFixed(1));
+    return fixed;
 };
-
+console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(73.2));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
