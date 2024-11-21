9108b4ddb5b000d50d272e0949d5e44302eef6b9
Juan Pineda <juancanales5019@gmail.com>
2024-11-21 16:21:03 -0600

    create pseudo code to create the function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // str to array
+    let arr; 
+    // take away punctuation and spaces.
 
+    // join array in normal order and store it in stringA
+
+    // join array in reversed order and store it in stringB
+
+    // compare if both string are the same
 };
 
 // Do not edit below this line
