a7b6804a874fc3027b748ec640e32ea4114ddf83
Juan Pineda <juancanales5019@gmail.com>
2024-11-26 08:50:17 -0600

    set up the test files in order to start coding

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,7 +1,17 @@
 const getTheTitles = function(obj) {
+    console.log(books);
     //map the object to look for any key related to tile;
     //select the title key to create the new array of title;
 };
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 // Do not edit below this line
-module.exports = getTheTitles;
+//module.exports = getTheTitles;
