3a243dd45d8c0682ef40d5c694690e19e315eac3
Juan Pineda <juancanales5019@gmail.com>
2024-11-26 17:05:53 -0600

    fix a bug, now it return an object to work on

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,4 @@
-const findTheOldest = function(obj=people) {
+const findTheOldest = function(obj) {
     //get ages of all people and include it in the object
     let ages = [];
     obj.forEach(element => {
@@ -7,35 +7,17 @@ const findTheOldest = function(obj=people) {
         //get and array of ages
         ages.push(element.age);
     });
-  
     //sort ages to determine the highest number which will be the last;
     let sortedArray = ages.sort();
     let biggestAge = sortedArray.pop();
     //return the oldes
+    let objElement = {};
     obj.forEach(element => {
-      if(biggestAge == element.age){
-        console.log(element.name);
-        return element.name;
-      }
+        if(biggestAge == element.age){
+          objElement = element;
+        }
     });
+    return objElement;
 };
-
-const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]
 // Do not edit below this line
-//module.exports = findTheOldest;
+module.exports = findTheOldest;
